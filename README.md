2a96542dfb205fac12e13224eb3e83c4cd676d58
Robert Jackson <robert.w.jackson@me.com>
2014-04-23 12:46:48 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,49 @@
+# Broccoli Export Tree
+
+Export a tree to an external directory.
+
+## Usage
+
+Export a tree to the `dist/` directory:
+
+```javascript
+var exportTree = require('broccoli-export-tree');
+
+// assuming someTree is a built up tree
+var tree = exportTree(someTree, {
+  destDir: 'dist'
+});
+```
+
+## Documentation
+
+### `exportTree(inputTree, options)`
+
+---
+
+`options.destDir` *{String}*
+
+The path to move the export to.
+
+---
+
+`options.clobber` *{true,false}*
+
+Should the destination directory be removed before copying?
+
+Default: *true*
+
+## ZOMG!!! TESTS?!?!!?
+
+I know, right?
+
+Running the tests:
+
+```javascript
+npm install
+npm test
+```
+
+## License
+
+This project is distributed under the MIT license.
---
+++ index.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+var path = require('path');
+var rimraf = require('rimraf');
+var Writer = require('broccoli-writer');
+var helpers = require('broccoli-kitchen-sink-helpers')
+
+ExportTree.prototype = Object.create(Writer.prototype);
+ExportTree.prototype.constructor = ExportTree;
+function ExportTree (inputTree, options) {
+  if (!(this instanceof ExportTree)) return new ExportTree(inputTree, options);
+
+  this.inputTree = inputTree;
+  this.destDir   = options.destDir;
+  this.clobber   = true;
+
+  if (options.hasOwnProperty('clobber')) { this.clobber = options.clobber; }
+};
+
+ExportTree.prototype.write = function (readTree, destDir) {
+  var self = this
+
+  return readTree(this.inputTree).then(function (srcDir) {
+    if (self.clobber) {
+      console.log('clobbering');
+      rimraf.sync(self.destDir);
+    }
+
+    helpers.copyRecursivelySync(srcDir, self.destDir);
+  })
+};
+
+module.exports = ExportTree;
