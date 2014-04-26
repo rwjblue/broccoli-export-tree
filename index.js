22d3c4791a93ed5aec68d7661433ef5d0bef20e6
Robert Jackson <robert.w.jackson@me.com>
2014-04-26 13:19:39 -0400

    Update to use broccoli-caching-writer.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var fs = require('fs');
 var path = require('path');
 var rimraf = require('rimraf');
-var Writer = require('broccoli-writer');
+var CachingWriter = require('broccoli-caching-writer');
 var helpers = require('broccoli-kitchen-sink-helpers')
 
-ExportTree.prototype = Object.create(Writer.prototype);
+ExportTree.prototype = Object.create(CachingWriter.prototype);
 ExportTree.prototype.constructor = ExportTree;
 function ExportTree (inputTree, options) {
   if (!(this instanceof ExportTree)) return new ExportTree(inputTree, options);
@@ -16,16 +16,12 @@ function ExportTree (inputTree, options) {
   if (options.hasOwnProperty('clobber')) { this.clobber = options.clobber; }
 };
 
-ExportTree.prototype.write = function (readTree, destDir) {
-  var self = this
+ExportTree.prototype.updateCache = function (srcDir, destDir) {
+  if (this.clobber) {
+    rimraf.sync(this.destDir);
+  }
 
-  return readTree(this.inputTree).then(function (srcDir) {
-    if (self.clobber) {
-      rimraf.sync(self.destDir);
-    }
-
-    helpers.copyRecursivelySync(srcDir, self.destDir);
-  })
+  helpers.copyRecursivelySync(srcDir, this.destDir);
 };
 
 module.exports = ExportTree;
